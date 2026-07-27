dab33850a2b79446bd0bbbdb5f093f26de9cd0c3
NicholasBrb <nick_flick@outlook.com>
2026-07-27 10:25:47 +0200

    v1.1.4

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://nicholasbarbieri.netlify.app',
+  site: 'https://nicholasbarbieri.xyz',
   vite: {
     plugins: [tailwindcss()]
   }
